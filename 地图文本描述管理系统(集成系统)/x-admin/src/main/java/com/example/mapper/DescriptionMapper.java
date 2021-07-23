bf2a3a6fb7ee547ddf59a011f674c78c4cb54a0d
cby6702 <459971019@qq.com>
2021-07-23 11:13:31 +0800

    重命名

--- 地图文本描述管理系统(集成系统)/x-admin/src/main/java/com/example/mapper/DescriptionMapper.java
+++
@@ -1,8 +0,0 @@
-package com.example.mapper;
-
-import com.example.entity.Description;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-
-public interface DescriptionMapper extends BaseMapper<Description> {
-
-}
--- 地图文本描述管理系统(集成系统)/x-admin/src/main/java/com/example/mapper/PositionMapper.java
+++
@@ -1,8 +0,0 @@
-package com.example.mapper;
-
-import com.example.entity.Position;
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-
-public interface PositionMapper extends BaseMapper<Position> {
-
-}
